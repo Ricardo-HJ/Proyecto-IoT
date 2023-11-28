3660c512f26c17f7a9b0af29636d0f7a5e63a6d2
Ricardo-HJ <a00837337@tec.mx>
2023-11-27 20:58:59 -0600

    Proyect upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export function LastReading({ CompID }) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://10.22.207.25:3100/api/getLastLog/${CompID}`)
+      .then(response => response.json())
+      .then(data => { setData(data.data); })
+      .catch(console.error);
+  }, [CompID]); // Include CompID in the dependency array
+
+  if (data) {
+    const lastData = data[data.length - 1];
+    const logValue = lastData.LogValue;
+
+    return (
+      <div>
+      {`${logValue}`}
+      </div>
+    );
+  }
+
+  return <div>0.00</div>;
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { LastReading } from './App.js';
 import reportWebVitals from './reportWebVitals';
+
+// SideMenu component
+const SideMenu = () => (
+  <div className="side-menu">
+    <img src={process.env.PUBLIC_URL + '/home.png'} alt="Home" className="icon-home"></img>
+    <img src={process.env.PUBLIC_URL + '/Mov.png'} alt="Movimiento" className="icon-mov"></img>
+    <img src={process.env.PUBLIC_URL + '/Peso.png'} alt="Peso" className="icon-peso"></img>
+    <img src={process.env.PUBLIC_URL + '/Sonido.png'} alt="Sonido" className="icon-Sonido"></img>
+    <img src={process.env.PUBLIC_URL + '/Temp.png'} alt="Temperatura" className="icon-Temp"></img>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <table className="main-container">
+      <tr>
+        <SideMenu />
+      </tr>
+      <tr>
+          <table>
+            <tr className="blocks-container">  
+              <td className="block">
+                <div className="block-title">
+                <img src={process.env.PUBLIC_URL + '/Icono de peso.png'} alt=""></img>
+                <p>Sensor de<br/>peso</p>
+                </div>
+                <div className='block-info'>
+                <h1><LastReading CompID='1'/></h1>
+                  <div>
+                    <h2>0.94 kg</h2>
+                    <p>Mas desde el ultimo mes</p>
+                  </div>
+                </div>
+              </td>
+              <td className="block">
+                <div className="block-title">
+                <img src={process.env.PUBLIC_URL + '/Icono de luz.png'} alt=""></img>
+                <p>Sensor de<br/>luz</p>
+                </div>
+                <div className='block-info'>
+                <h1><LastReading CompID='9'/></h1>
+                  <div>
+                    <h2>-12lm</h2>
+                    <p>Que el promedio a esta hora</p>
+                  </div>
+                </div>
+              </td>
+              <td className="block">
+                <div className="block-title">
+                <img src={process.env.PUBLIC_URL + '/Icono sonido.png'} alt=""></img>
+                <p>Sensor de<br/>sonido</p>
+                </div>
+                <div className='block-info'>
+                <h1><LastReading CompID='5'/></h1>
+                  <div>
+                    <h2>{''}</h2>
+                    <p>Hay mucho ruido</p>
+                  </div>
+                </div>
+              </td>
+              <td className="block">
+                <div className="block-title">
+                <img src={process.env.PUBLIC_URL + '/Icono mov.png'} alt=""></img>
+                <p>Sensor de<br/>movimiento</p>
+                </div>
+                <div className='block-info'>
+                  <h3><LastReading CompID='7'/></h3>
+                  <div>
+                    <p>Desde hace 15 min</p>
+                  </div>
+                </div>
+              </td>
+              <td className="block">
+                <div className="block-title">
+                <img src={process.env.PUBLIC_URL + '/Icono temp.png'} alt=""></img>
+                <p>Sensor de <br/>temperatura</p>
+                </div>
+                <div className='block-info'>
+                  <h1><LastReading CompID='3'/></h1>
+                  <div>
+                    <h2>{''}</h2>
+                    <p>Temperatura mas alta del dia</p>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
+      </tr>
+    </table>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
